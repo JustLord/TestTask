c332fb3124be7f53da3adb9224b356564d11f447
nikita <justlordmlg@gmail.com>
2019-12-01 14:04:47 +0300

    output format

--- writers/ScheduleWriter.cpp
+++ writers/ScheduleWriter.cpp
@@ -7,17 +7,17 @@
 void ScheduleWriter::write(const std::string &fileName, const std::vector<Schedule> &schedules) {
     file.open(fileName, std::ios::out);
 
-    for (auto &schedule : schedules)
-        writeLine(schedule);
+    for (unsigned i = 0; i < schedules.size(); i++) {
+        if (i && schedules[i - 1].company != schedules[i].company)
+            file << std::endl;
+
+        writeLine(schedules[i]);
+    }
 
     file.close();
 }
 
 void ScheduleWriter::writeLine(const Schedule &schedule) {
-    file << schedule.company->name;
-    file << " ";
-    file << schedule.departureTime.toString();
-    file << " ";
-    file << schedule.arrivalTime.normalised().toString();
-    file << std::endl;
+    file << schedule.company->name << " " << schedule.departureTime.toString() << " "
+         << schedule.arrivalTime.normalised().toString() << std::endl;
 }
