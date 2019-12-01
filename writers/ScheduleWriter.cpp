6e5bc2d928b989deed51beb9932ba49f6978480c
nikita <justlordmlg@gmail.com>
2019-12-01 14:19:09 +0300

    Save error message added

--- writers/ScheduleWriter.cpp
+++ writers/ScheduleWriter.cpp
@@ -2,19 +2,26 @@
 // Created by nikita on 11/30/19.
 //
 
+#include <iostream>
 #include "ScheduleWriter.h"
 
 void ScheduleWriter::write(const std::string &fileName, const std::vector<Schedule> &schedules) {
     file.open(fileName, std::ios::out);
 
-    for (unsigned i = 0; i < schedules.size(); i++) {
-        if (i && schedules[i - 1].company != schedules[i].company)
-            file << std::endl;
+    if (file.is_open()) {
+        for (unsigned i = 0; i < schedules.size(); i++) {
+            if (i && schedules[i - 1].company != schedules[i].company)
+                file << std::endl;
 
-        writeLine(schedules[i]);
+            writeLine(schedules[i]);
+        }
+
+        file.close();
+
+        std::cout << "Result saved on output.txt" << std::endl;
+    } else {
+        std::cout << "Error while saving result" << std::endl;
     }
-
-    file.close();
 }
 
 void ScheduleWriter::writeLine(const Schedule &schedule) {
